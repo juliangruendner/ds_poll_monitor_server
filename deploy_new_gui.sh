ff2114bb2d756559a8e22041a77acf8142854884
Julian Gründner <juliangruendner@googlemail.com>
2018-09-11 14:35:12 +0200

    add deploy gui sh and start_prod sh

---
+++ deploy_new_gui.sh
@@ -0,0 +1,3 @@
+rm -rf ds_poll_gui/dist
+unzip ../ds_poll_monitor_gui/poll_gui.zip -d  ds_poll_gui
+cp -R ds_poll_gui/dist/* ds_poll_gui/poll-monitor
---
+++ start_prod.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose.prod.yml -p poll_monitor up -d
