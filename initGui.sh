5b3fd8698e35138521d352cc82b3594526652364
Julian Gründner <juliangruendner@googlemail.com>
2018-12-12 11:04:37 +0100

    add configurations to support dockerfile artefacts for production

--- initGui.sh
+++ initGui.sh
@@ -1,3 +1,5 @@
+cp -R /dist/* /usr/local/apache2/htdocs/poll-monitor
+
 cd /usr/local/apache2/htdocs/poll-monitor && \
     sed -i "s/<replace_prod_server_url>/$OPAL_SERVER_IP/g" main.*
 
--- start_prod.sh
+++ start_prod.sh
@@ -1,1 +1,2 @@
+source ../opal_poll.config
 docker-compose -f docker-compose.prod.yml -p poll_monitor up -d
